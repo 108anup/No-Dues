8af6d3ca568374fa6282317171b3be09a1e33a66
suhas0208 <suhas.kantekar.98@gmail.com>
2016-11-15 19:41:19 +0530

    Added dues boolean and set to false as default

--- database/seeds/StudentTableSeeder.php
+++ database/seeds/StudentTableSeeder.php
@@ -12,15 +12,27 @@ class StudentTableSeeder extends Seeder
      */
     public function run()
     {
-        //TODO:: Suhas Add Dues
+        //Dues added as boolean
         DB::table('student')->insert([
             'name' => 'Suhas Kantekar',
             'email' => 'suhas.kantekar@iitg.ernet.in',
             'dept' => 'cse',
             'hostel' => 'barak',
+            'care_taker' => false,
+            'gymkhana' => false,
+            'submit_thesis' => false,
+            'online_no_dues_for_cc' => false,
+            'deptno_due_and_workshop' => false,
+            'warden' => false,
+            'library' => false,
+            'cc_in_charge' => false,
+            'asst_registrar' => false,
+            'hod' => false,
+            'account' => false,
             'superviser_email' => 'pkdas@iitg.ernet.in',
             'created_at' => date("Y-m-d H:i:s"),
             'updated_at' => date("Y-m-d H:i:s"),
         ]);
     }
 }
+
