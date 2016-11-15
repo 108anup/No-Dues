b7eda780fdec87057d9b8647b35b2c9ae2bf5b1f
Anup Agarwal <108anup@gmail.com>
2016-11-15 17:26:34 +0000

    Minor Change in Staff Model

--- app/Staff.php
+++ app/Staff.php
@@ -7,10 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class Staff extends Model
 {
 	protected $table='staff';
-
-	//Type case the role attribute as an array of strings from json
-	//Did this because cannot declare an array of strings using eloquent
-	protected $casts = [
-		'role' => 'array'
-	];
 }
